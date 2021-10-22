16dee2868f8a585ee8133bf9fae5b75ab0b30f80
Maxence <maxencedupin001@gmail.com>
2021-10-22 14:25:33 +0200

    Add Player Jump
    - Update "PlayerMovement.cs" (add jump feature)
    - Add prefab "Player"

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,9 +4,18 @@ public class PlayerMovement : MonoBehaviour
 {
 
   public float moveSpeed;
+  public float jumpForce;
+
+  private bool isJumping;
+  [SerializeField] private bool isGrounded;
 
   private float horizontalMovement;
+  private float verticalMovement;
   private Vector3 velocity = Vector3.zero;
+
+  public Transform groundCheck;
+  public float groundCheckRadius;
+  public LayerMask collisionLayers;
 
   public Rigidbody2D rb;
   public SpriteRenderer spriteRenderer;
@@ -15,6 +24,11 @@ public class PlayerMovement : MonoBehaviour
   {
     horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.fixedDeltaTime;
 
+    if(Input.GetButtonDown("Jump") && isGrounded)
+    {
+      isJumping = true;
+    }
+
     Flip(rb.velocity.x);
 
     float characterVelocity = Mathf.Abs(rb.velocity.x);
@@ -22,6 +36,7 @@ public class PlayerMovement : MonoBehaviour
 
   void FixedUpdate()
   {
+    isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, collisionLayers);
     MovePlayer(horizontalMovement);
   }
 
@@ -29,6 +44,12 @@ public class PlayerMovement : MonoBehaviour
   {
     Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
     rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+
+    if(isJumping == true)
+    {
+      rb.AddForce(new Vector2(0f, jumpForce));
+      isJumping = false;
+    }
   }
 
   void Flip(float _velocity)
@@ -41,5 +62,11 @@ public class PlayerMovement : MonoBehaviour
     {
       spriteRenderer.flipX = true;
     }
+  }
+
+  private void OnDrawGizmos()
+  {
+    Gizmos.color = Color.red;
+    Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
   }
 }
